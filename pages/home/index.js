eae93956e57ef05722a6beb57b64a019ba67fe10
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2022-12-15 17:38:54 -0300

    criou-se uma nova chave no local storage e realizou-se uma função para adicionar e remover os objetos do array do local storage

--- pages/home/index.js
+++ pages/home/index.js
@@ -43,10 +43,10 @@ function createCardJobsList (element) {
     return li;
 }
 
-function renderCardJobsList() {
+function renderCardJobsList(array) {
     const jobsList = document.querySelector('#jobs__list');
 
-    jobsData.forEach (job => {
+    array.forEach (job => {
         let jobsCards = createCardJobsList(job);
 
         jobsList.append(jobsCards);
@@ -54,7 +54,87 @@ function renderCardJobsList() {
 }
 
 function createCardApplyJobsLIst (element) {
-    
+    let li = document.createElement('li');
+    li.id = element.id;
+
+    let applyJobsListTitle = document.createElement('div');
+    applyJobsListTitle.classList.add('apply-jobs__list--title');
+
+    let title = document.createElement('h3');
+    title.innerText = element.title;
+
+    let removeJob = document.createElement('button');
+    removeJob.classList.add('remove-button');
+    removeJob.dataset.id = element.id;
+
+    let buttonImage = document.createElement('img');
+    buttonImage.src = "/assets/img/trash-icon.svg";
+
+    removeJob.append(buttonImage);
+
+    applyJobsListTitle.append(title, removeJob);
+
+    let applyJobsListLocal = document.createElement('div');
+    applyJobsListLocal.classList.add('apply-jobs__list--local');
+
+    let spanLocal1 = document.createElement('span');
+    let spanLocal2 = document.createElement('span');
+    spanLocal1.classList.add('span1');
+    spanLocal2.classList.add('span2');
+    spanLocal1.innerText = element.enterprise;
+    spanLocal2.innerText = element.location;
+
+    applyJobsListLocal.append(spanLocal1, spanLocal2);
+
+    li.append(applyJobsListTitle, applyJobsListLocal);
+
+    return li;
 }
 
-renderCardJobsList()
+function applyJobsContainer () {
+    const sectionApplyJobs = document.querySelector('.main__section--apply-jobs');
+    const ulApplyJobs = document.querySelector('#apply-jobs__list');
+    if (ulApplyJobs.children.length === 0) {
+        let p = document.createElement('p');
+        p.classList.add('apply-jobs-empty');
+        p.innerText = 'Você ainda não aplicou para nenhuma vaga';
+
+        sectionApplyJobs.appendChild(p);
+    } 
+}
+
+function getApplyJobsArray() {
+    return JSON.parse(localStorage.getItem('@Apply-jobs')) || [];
+}
+
+function findJobsById (array, id) {
+    
+    return array.find(job => job.id == id);
+}
+
+function addAndRemoveJobs () {
+    let applyJobsArray = getApplyJobsArray();
+    let jobsListButtons = document.querySelectorAll('.jobs__list--button');
+
+    jobsListButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            let buttonId = e.target.dataset.id;
+            let jobFound = findJobsById(jobsData, buttonId);
+            
+            if (!applyJobsArray.includes(jobFound)) {
+                applyJobsArray.push(jobFound);
+                button.innerText = 'Remover candidatura';
+                localStorage.setItem('@Apply-jobs', JSON.stringify(applyJobsArray));
+            } else {
+                let jobIndex = applyJobsArray.indexOf(jobFound);
+                applyJobsArray.splice(jobIndex, 1);
+                button.innerText = 'Candidatar';
+                localStorage.setItem('@Apply-jobs', JSON.stringify(applyJobsArray));
+            }
+        })
+    })
+}
+
+renderCardJobsList(jobsData);
+applyJobsContainer();
+addAndRemoveJobs ();
